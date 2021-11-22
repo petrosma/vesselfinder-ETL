8648ebfba63ef6d2c8160c1854bb4a9c39baf7c7
Mahdi Alavi <52528150+petrosma@users.noreply.github.com>
2021-11-22 16:02:30 +0330

    Update db.py

--- db.py
+++ db.py
@@ -6,8 +6,6 @@ from sqlalchemy.orm import sessionmaker
 SQLALCHEMY_DATABASE_URL = "sqlite:///./vessels.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
-#SQLALCHEMY_DATABASE_URL = "mysql://Ln1ki1vjdf:2hMrrqsHGn@remotemysql.com/Ln1ki1vjdf"
-#SQLALCHEMY_DATABASE_URL = "postgres://645297:173238235@localhost/WIMS"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
